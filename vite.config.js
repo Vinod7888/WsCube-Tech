c82069b19c5838daffd5f8ddd09cc56cd931a9ec
vinod prajapat <vinodprajapat7888@gmail.com>
2025-11-05 13:50:48 +0530

    add new code in vite config file me

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//   ],
+//   base: '/WsCube-Tech/',
+// })
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-  base: '/WsCube-Tech/',
+  plugins: [react(), tailwindcss()],
+  base: '/WsCube-Tech/', // 👈 yaha tera repo name
 })
